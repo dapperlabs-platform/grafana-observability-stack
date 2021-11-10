7935e6c803d462ff318d09d0292c997327a37329
haroldsphinx <adedayoakinpelu@gmail.com>
2021-11-10 12:32:56 +0100

    Update Readme

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 This module creates a grafana synthetic monitor that takes the job name and the target url as parameters
 
-## To use this module, create a resource, and add the target_url e.g "www.dapperlabs.com" and the job_name, e.g "Dapperlabs Monitor" as shown below
+## To use this module, create a resource, and add the target_url e.g "www.example.com" and the job_name, e.g "Example Monitor" as shown below
 
 ```hcl
 module "grafana-monitoring" {
     source = "../Shared/modules/grafana"
     job_name = "Flow Docs"
-    cf_zone_name = "https://docs.onflow.org"
+    cf_zone_name = "https://example.com"
 }
 ```
 
--- variables.tf
+++ variables.tf
@@ -1,63 +1,14 @@
-variable "monitoring_data" {
-    description = "A map of target_url, job_name and project_label for Synthetic monitoring"
-    type        = map(list(string))
+variable "job_name" {
+  description = "This is the name you want to give to your Synthetic Monitor"
+  type        = map(list(string))
 }
 
-# variable "job_name" {
-#   description = "This is the name you want to give to your Synthetoc Monitor"
-#   type        = map(list(string))
-# }
+variable "target_url" {
+  description = "This is the http url for the website endpoint you want to monitor"
+  type        = map(list(string))
+}
 
-# variable "target_url" {
-#   description = "This is the http url for the website endpoint you want to monitor"
-#   type        = map(list(string))
-# }
-
-# variable "record_name" {
-#     description = "The record name to create on cloudflare. e.g <record_name>.dapperlabs.com"
-#     type = string
-#     default = ""
-# }
-
-# variable "cf_zone_name" {
-#     description = "The zone name if not automatically generated from the datasource, e.g dapperlabs.com"
-#     type = string
-#     default = ""
-# }
-
-# variable "grafana_url" {
-#     description = "Grafana URL"
-#     type = string
-#     default = "https://dapperlabs.grafana.net/"
-# }
-
-# variable "grafana_folder_title" {
-#     description = "Grafana Folder Name"
-#     type = string
-# }
-
-# variable "dashboard_config_file" {
-#     description = "Path to Grafana Dashboard Config FIle"
-#     type = string
-# }
-
-
-# variable "datasource_type" {
-#     description = "Grafana datasource type"
-#     type = string
-# }
-
-# variable "datasource_url" {
-#     description = "Grafana datasource url"
-#     type = string
-# }
-
-# variable "datasource_name" {
-#     description = "Grafana datasource Name"
-#     type = string
-# }
-
-# variable "project_label" {
-#     description = "Project Label"
-#     type = string
-# }
+variable "project_label" {
+    description = "Project Label"
+    type = string
+}
